63ae0dd7c424702d03a10e9489ff6026e2890f18
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-06-12 07:05:15 -0700

    Updated the User.php function

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,20 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable; // Alterado
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
-class User extends Model
+class User extends Authenticatable
 {
-    protected $fillable = ['name', 'email'];
+    public $timestamps = false;
+    protected $fillable = ['name', 'email', 'password']; // lembre de adicionar 'password'
 
     public static function createUser(array $data): JsonResponse
     {
         $data['user_password'] = Hash::make($data['user_password']);
 
-        $user = DB::table("users")->insert(['name' => $data['user_name'], 'email' => $data['user_email'], 'password' => $data['user_password']]);
+        $userId = DB::table("users")->insertGetId([
+            'name' => $data['user_name'], 
+            'email' => $data['user_email'], 
+            'password' => $data['user_password']
+        ]);
+
+        $user = self::find($userId);
 
         return response()->json($user, 201);
     }
