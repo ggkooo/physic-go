4aa3dcd94a17dd230e7ba847fd9cef6e6ea0d73f
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-06-13 22:01:24 -0300

    email.blade.php: Page to user insert email to reset password. reset.blade.php: Page to change password. confirm.blade.php: not implemented yet, but this is to verify user autenticity. ForgotPasswordController.php: Verify email and send the email to user. ResetPasswordControlle.php: update and validate the password to change.

---
+++ resources/views/auth/passwords/confirm.blade.php
@@ -0,0 +1,54 @@
+@extends('layouts.app')
+
+@section('content')
+
+<!-- 
+    Quando o usuário já está logado, mas precisa confirmar a senha para acessar uma área sensível (exemplo: alterar dados pessoais, informações de pagamento, ou algo que exige segurança extra), o Laravel pode pedir para ele confirmar sua senha atual antes de continuar.
+-->
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Confirm Password') }}</div>
+
+                <div class="card-body">
+                    {{ __('Please confirm your password before continuing.') }}
+
+                    <form method="POST" action="{{ route('password.confirm') }}">
+                        @csrf
+
+                        <div class="row mb-3">
+                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+
+                                @error('password')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Confirm Password') }}
+                                </button>
+
+                                @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        {{ __('Forgot Your Password?') }}
+                                    </a>
+                                @endif
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
