73e0ce5d5dd7d9a283db07b9325858caa4841eb1
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-07-28 22:26:27 -0300

    fix: updated the user factory to add user_account_type

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,7 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'user_account_type' => fake()->randomElement(['Aluno', 'Professor']), // Adiciona tipo de conta
         ];
     }
 
