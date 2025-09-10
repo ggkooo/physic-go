4a9a9e8fb91b29faa78e0e816d4f92e4bb7a2358
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-10 10:11:36 -0300

    refactor: refactored all application routes

--- routes/web.php
+++ routes/web.php
@@ -15,96 +15,184 @@ use App\Http\Controllers\ManagementController;
 use App\Http\Controllers\Management\SchoolController;
 use App\Http\Controllers\Management\UserController;
 
-// USER
-Route::get('/users/login', [LoginController::class, 'showLoginForm'])->name('login');
-Route::post('/users/login', [LoginController::class, 'login'])->name('login.perform');
-Route::get('/users/logout', [LoginController::class, 'logout'])->name('logout');
+// AUTHENTICATION ROUTES
+    // Login
+    Route::get('/users/login', [LoginController::class, 'showLoginForm'])->name('login');
+    Route::post('/users/login', [LoginController::class, 'login'])->name('login.submit');
 
-// Registro
-Route::get('/users/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
-Route::post('/users/register', [RegisterController::class, 'register'])->name('register.perform');
+    // Register
+    Route::get('/users/register', [RegisterController::class, 'showRegistrationForm'])->name('register');
+    Route::post('/users/register', [RegisterController::class, 'register'])->name('register.submit');
 
-// Esqueci a senha
-Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.request');
-Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+    // Logout
+    Route::get('/users/logout', [LoginController::class, 'logout'])->name('logout');
 
-Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset');
-Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
+    // Password Reset
+    Route::get('password/reset', [ForgotPasswordController::class, 'showLinkRequestForm'])->name('password.reset');
+    Route::post('password/email', [ForgotPasswordController::class, 'sendResetLinkEmail'])->name('password.email');
+    Route::get('password/reset/{token}', [ResetPasswordController::class, 'showResetForm'])->name('password.reset.token');
+    Route::post('password/reset', [ResetPasswordController::class, 'reset'])->name('password.update');
 
-// GOOGLE AUTH
-Route::get('auth/google', [GoogleController::class, 'redirectToGoogle'])->name('google.login');
-Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
+    // Google
+    Route::get('auth/google', [GoogleController::class, 'redirectToGoogle'])->name('google.login');
+    Route::get('auth/google/callback', [GoogleController::class, 'handleGoogleCallback']);
 
-// HOME
-Route::get('/', function () {
-    return redirect()->route('home');
-});
+// ACCOUNT
+    Route::get('/account', [ConfigAccountController::class, 'index'])->name('config.account');
+    Route::post('/account', [ConfigAccountController::class, 'update'])->name('config.account.update');
 
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/credits', [HomeController::class, 'credits']);
+// PAGES ROUTES
+    // Redirect root to /home
+    Route::get('/', function () {
+        return redirect()->route('home');
+    });
 
-// CONFIG ACCOUNT
-Route::get('/account', [ConfigAccountController::class, 'index'])->name('config.account');
-Route::post('/account', [ConfigAccountController::class, 'update'])->name('config.account.update');
+    // Home
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-// CONTACT
-Route::get('/contact', [ContactController::class, 'index'])->name('contact');
+    // Credits
+    Route::get('/credits', [HomeController::class, 'credits'])->name('credits');
 
-// STUDY
-Route::get('/study', [StudyController::class, 'index'])->name('study');
+    // Contact
+    Route::get('/contact', [ContactController::class, 'index'])->name('contact');
 
-// GAME
-Route::get('/game/menu', [GameController::class, 'menu']);
-Route::get('/game/new', [GameController::class, 'new']);
-Route::get('/game/display', [GameController::class, 'display']);
-Route::get('/game/students_ranking', [GameController::class, 'students_ranking']);
-Route::get('/game/questions-by-serie', [GameController::class, 'questionsBySerie'])->name('game.questionsBySerie');
-Route::post('/game/save-ranking', [GameController::class, 'saveRanking'])->name('game.saveRanking');
+    // Study
+    Route::get('/study', [StudyController::class, 'index'])->name('study');
+
+    // Game
+    Route::get('/game/menu', [GameController::class, 'menu'])->name('game.menu');
+    Route::get('/game/new', [GameController::class, 'new'])->name('game.new');
+    Route::get('/game/display', [GameController::class, 'display'])->name('game.display');
+    Route::get('/game/students-ranking', [GameController::class, 'studentsRanking'])->name('game.students-ranking');
+    Route::get('/game/questions-by-grade', [GameController::class, 'questionsByGrade'])->name('game.questions-by-grade');
+    Route::post('/game/save-ranking', [GameController::class, 'saveRanking'])->name('game.save-ranking');
 
 // MANAGEMENT
-// home
-Route::get('/management/home', [ManagementController::class, 'home']);
+    // Home
+    Route::get('/management/home', [ManagementController::class, 'home'])->name('management.home');
 
-// publications
-Route::get('/management/publications', [ManagementController::class, 'publications']);
+    // Publications
+    Route::get('/management/publications', [ManagementController::class, 'publications'])->name('management.publications');
 
-// questions
-Route::get('/management/questions', [ManagementController::class, 'questions'])->name('management.questions');
-Route::get('/management/questions/register', [ManagementController::class, 'questionsRegister'])->name('management.questionsRegister');
-Route::post('/management/questions/register', [ManagementController::class, 'questionsStore'])->name('management.questions.store');
-Route::get('/management/questions/edit/{id}', [ManagementController::class, 'questionsEdit'])->name('management.questions.edit');
-Route::get('/management/questions/view/{id}', [ManagementController::class, 'questionsView'])->name('management.questions.view');
-Route::put('/management/questions/update/{id}', [ManagementController::class, 'questionsUpdate'])->name('management.questions.update');
-Route::get('/management/questions/statistics/{id}', [ManagementController::class, 'questionsStatistics'])->name('management.questions.statistics');
+    // Schools
+        // show
+        Route::get('/management/schools', [SchoolController::class, 'schools'])->name('management.schools');
 
-// schools
-Route::get('/management/schools', [SchoolController::class, 'schools'])->name('management.schools');
-Route::get('/management/schoolsRegister', [SchoolController::class, 'schoolsRegister'])->name('management.schools.register');
-Route::post('/management/schoolsRegister', [SchoolController::class, 'schoolsStore'])->name('management.schools.store');
-Route::delete('/management/schools/remove/{id}', [SchoolController::class, 'removeSchool'])->name('management.schools.remove');
-Route::get('/management/schools/edit/{id}', [SchoolController::class, 'editSchool'])->name('management.schools.edit');
-Route::put('/management/schools/update/{id}', [SchoolController::class, 'updateSchool'])->name('management.schools.update');
+        // register
+        Route::get('/management/schools-register', [SchoolController::class, 'schoolsRegister'])->name('management.schools.register');
+        Route::post('/management/schools-register', [SchoolController::class, 'schoolsStore'])->name('management.schools.store');
 
-// messages
-Route::get('/management/messages', [ManagementController::class, 'messages']);
+        // edit, update, remove
+        Route::delete('/management/schools/remove/{id}', [SchoolController::class, 'removeSchool'])->name('management.schools.remove');
+        Route::get('/management/schools/edit/{id}', [SchoolController::class, 'editSchool'])->name('management.schools.edit');
+        Route::put('/management/schools/update/{id}', [SchoolController::class, 'updateSchool'])->name('management.schools.update');
 
-// template
-Route::get('/management/template', [ManagementController::class, 'template']);
+    // Messages
+    Route::get('/management/messages', [ManagementController::class, 'messages'])->name('management.messages');
 
-// statistics
-Route::get('/management/statistics', [ManagementController::class, 'statistics']);
+    // Statistics
+    Route::get('/management/statistics', [ManagementController::class, 'statistics'])->name('management.statistics');
 
-// question/level
-Route::get('/management/questionsLevel', [ManagementController::class, 'questionsLevel']);
-Route::get('/management/questionsContent', [ManagementController::class, 'questionsContent']);
+    // Questions
+        // show
+        Route::get('/management/questions', [ManagementController::class, 'questions'])->name('management.questions');
 
-// challenge
-Route::get('/management/challenge', [ManagementController::class, 'challenge']);
-Route::get('/management/challengeRegister', [ManagementController::class, 'challengeRegister']);
+        // register
+        Route::get('/management/questions/register', [ManagementController::class, 'questionsRegister'])->name('management.questionsRegister');
+        Route::post('/management/questions/register', [ManagementController::class, 'questionsStore'])->name('management.questions.store');
 
-// USERS
-Route::get('/management/users', [UserController::class, 'users'])->name('management.users');
-Route::get('/management/users/edit/{id}', [UserController::class, 'editUser'])->name('management.users.edit');
-Route::put('/management/users/update/{id}', [UserController::class, 'updateUser'])->name('management.users.update');
-Route::delete('/management/users/remove/{id}', [UserController::class, 'removeUser'])->name('management.users.remove');
+        // edit
+        Route::get('/management/questions/edit/{id}', [ManagementController::class, 'questionsEdit'])->name('management.questions.edit');
+        Route::put('/management/questions/update/{id}', [ManagementController::class, 'questionsUpdate'])->name('management.questions.update');
+
+        // remove
+        Route::delete('/management/questions/remove/{id}', [ManagementController::class, 'questionsRemove'])->name('management.questions.remove');
+
+        // view one only
+        Route::get('/management/questions/view/{id}', [ManagementController::class, 'questionsView'])->name('management.questions.view');
+
+        // view one statistics only
+        Route::get('/management/questions/statistics/{id}', [ManagementController::class, 'questionsStatistics'])->name('management.questions.statistics');
+
+    // Grade
+        // show
+        Route::get('/management/grades', [ManagementController::class, 'grades'])->name('management.grades');
+
+        // register
+        Route::get('/management/grades/register', [ManagementController::class, 'gradesRegister'])->name('management.grades.register');
+        Route::post('/management/grades/register', [ManagementController::class, 'gradesStore'])->name('management.grades.store');
+
+        // edit
+        Route::get('/management/grades/edit/{id}', [ManagementController::class, 'gradesEdit'])->name('management.grades.edit');
+        Route::put('/management/grades/update/{id}', [ManagementController::class, 'gradesUpdate'])->name('management.grades.update');
+
+        // remove
+        Route::delete('/management/grades/remove/{id}', [ManagementController::class, 'gradesRemove'])->name('management.grades.remove');
+
+    // Content
+        // show
+        Route::get('/management/contents', [ManagementController::class, 'contents'])->name('management.contents');
+
+        // register
+        Route::get('/management/contents/register', [ManagementController::class, 'contentsRegister'])->name('management.contents.register');
+        Route::post('/management/contents/register', [ManagementController::class, 'contentsStore'])->name('management.contents.store');
+
+        // edit
+        Route::get('/management/contents/edit/{id}', [ManagementController::class, 'contentsEdit'])->name('management.contents.edit');
+        Route::put('/management/contents/update/{id}', [ManagementController::class, 'contentsUpdate'])->name('management.contents.update');
+
+        // remove
+        Route::delete('/management/contents/remove/{id}', [ManagementController::class, 'contentsRemove'])->name('management.contents.remove');
+
+    // Template
+        // show
+        Route::get('/management/template', [ManagementController::class, 'template'])->name('management.template');
+
+        // register
+        Route::get('/management/template/register', [ManagementController::class, 'templateRegister'])->name('management.template.register');
+        Route::post('/management/template/register', [ManagementController::class, 'templateStore'])->name('management.template.store');
+
+        // edit
+        Route::get('/management/template/edit/{id}', [ManagementController::class, 'templateEdit'])->name('management.template.edit');
+        Route::put('/management/template/update/{id}', [ManagementController::class, 'templateUpdate'])->name('management.template.update');
+
+        // remove
+        Route::delete('/management/template/remove/{id}', [ManagementController::class, 'templateRemove'])->name('management.template.remove');
+
+    // Users
+        // show
+        Route::get('/management/users',  [ManagementController::class, 'users'])->name('management.users');
+
+        // edit
+        Route::get('/management/users/edit/{id}', [ManagementController::class, 'usersEdit'])->name('management.users.edit');
+        Route::put('/management/users/update/{id}', [ManagementController::class, 'usersUpdate'])->name('management.users.update');
+
+        // remove
+        Route::delete('/management/users/remove/{id}', [ManagementController::class, 'usersRemove'])->name('management.users.remove');
+
+    // Challenge
+        // show
+        Route::get('/management/challenge', [ManagementController::class, 'challenge'])->name('management.challenge');
+
+        // register
+        Route::get('/management/challenge/register', [ManagementController::class, 'challengeRegister'])->name('management.challenge.register');
+        Route::post('/management/challenge/register', [ManagementController::class, 'challengeStore'])->name('management.challenge.store');
+
+        // edit
+        Route::get('/management/challenge/edit/{id}', [ManagementController::class, 'challengeEdit'])->name('management.challenge.edit');
+        Route::put('/management/challenge/update/{id}', [ManagementController::class, 'challengeUpdate'])->name('management.challenge.update');
+
+        // remove
+        Route::delete('/management/challenge/remove/{id}', [ManagementController::class, 'challengeRemove'])->name('management.challenge.remove');
+
+        // questions in a challenge
+            // show
+            Route::get('/management/challenge/{id}/questions', [ManagementController::class, 'challengeQuestions'])->name('management.challenge.questions');
+
+            // add question to challenge
+            Route::get('/management/challenge/{id}/questions/add', [ManagementController::class, 'challengeQuestionsAdd'])->name('management.challenge.questions.add');
+            Route::post('/management/challenge/{id}/questions/add', [ManagementController::class, 'challengeQuestionsStore'])->name('management.challenge.questions.store');
+
+            // remove question from challenge
+            Route::delete('/management/challenge/{challenge_id}/questions/remove/{question_id}', [ManagementController::class, 'challengeQuestionsRemove'])->name('management.challenge.questions.remove');
 
