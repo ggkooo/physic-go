27ba074451866285a9ea55fb9cc0c2061de65e3c
Giordano Bruno Biasi Berwig <giordanoberwig@proton.me>
2025-09-11 07:56:07 -0300

    fix: updated contact page link routes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,18 +1,15 @@
 <div class="d-flex flex-column align-items-center mt-4">
     <div class="col-md-10 d-flex justify-content-between">
-
         <div class="col-md-6 p-3">
             <div class="card card-contact mb-4 p-2">
                 <div class="card-body d-flex justify-content-center">
                     <div class="row col-md-12">
                         <span class="mb-3">Desenvolvedores</span>
-
                         <div class="mb-3">
                             Cássia Polleto
                             <a href="mailto:cassiapolleto@gmail.com"
                                 class="text-email text-decoration-none small">cassiapolleto@gmail.com</a>
                         </div>
-
                         <div class="mb-3">
                            Giordano Berwig
                             <a href="mailto:giordanoberwig@proton.me"
@@ -21,43 +18,32 @@
                     </div>
                 </div>
             </div>
-
             <div class="card card-contact mb-4 p-2">
                 <div class="card-body d-flex justify-content-center">
                     <div class="row col-md-12">
-                        <span class="mb-3">Professor Coordenador</span>
-
+                        <span class="mb-3">Professores Coordenadores</span>
                         <div class="mb-3">
                             Edson Padoin
-                            <a href="mailto:padoin@unijui.edu.br"
-                                class="text-email text-decoration-none small">padoin@unijui.edu.br</a>
+                            <a href="mailto:padoin@unijui.edu.br" class="text-email text-decoration-none small">padoin@unijui.edu.br</a>
                         </div>
-
                         <div class="mb-3">
-                            Barbara Gundel
-                            <a href="mailto:barbara.gundel@unijui.edu.br"
-                                class="text-email text-decoration-none small">barbara.gundel@unijui.edu.br</a>
+                            Barbara Gündel
+                            <a href="mailto:barbara.gundel@unijui.edu.br" class="text-email text-decoration-none small">barbara.gundel@unijui.edu.br</a>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-
-
         <div class="col-md-6 p-3">
             <div class="card card-border mb-4">
                 <div class="card-body d-flex justify-content-center p-4">
                     <div class="row">
-
                         <div class="col-sm-12 mb-3 mt-2 text-center">
                             Entre em Contato
                         </div>
-
-
                         <div class="col-sm-12 mb-3 mt-3">
                             <form action="#" method="POST">
                                 @csrf
-
                                 <div class="row">
                                     <div class="col">
                                         <div class="mb-3">
@@ -74,7 +60,6 @@
                                         </div>
                                     </div>
                                 </div>
-
                                 <div class="row">
                                     <div class="col">
                                         <div class="mb-3">
@@ -91,7 +76,6 @@
                                         </div>
                                     </div>
                                 </div>
-
                                 <div class="row">
                                     <div class="col">
                                         <div class="mb-3">
@@ -107,30 +91,24 @@
                                         </div>
                                     </div>
                                 </div>
-
                                 <div class="row mt-2">
                                     <div class="input-group">
                                         <button type="submit"
                                             class="btn btn-danger btn-default w-100 p-2">Enviar</button>
                                     </div>
                                 </div>
-
                             </form>
                         </div>
-
                     </div>
                 </div>
             </div>
         </div>
-
     </div>
-
     <div class="row">
-        <a href="/home"
+        <a href="{{ route('home') }}"
             class="btn-exit text-decoration-none text-white w-100 p-1 position-relative d-inline-block overflow-hidden mt-4 mb-2s">
             <i class="bi bi-arrow-left icon" style="font-size: 20px; margin-top: 2px;"></i>
             <span class="exit-text" style="font-size: 18px;">Voltar</span>
         </a>
     </div>
-
 </div>
