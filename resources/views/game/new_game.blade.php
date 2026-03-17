ea3e9b13cffa627979e7658acc1e9038529e4473
Cássia Polleto <cassiapolleto@gmail.com>
2026-03-17 08:11:37 -0300

    feat: User interface improvement for the series selection page

--- resources/views/game/new_game.blade.php
+++ resources/views/game/new_game.blade.php
@@ -1,44 +1,75 @@
-<div class="d-flex flex-column align-items-center mt-4">
-    <div class="col-md-9 mb-2">
-        <div class="d-flex justify-content-center mb-4">
-            <h5>ENSINO FUNDAMENTAL</h5>
+<div class="container d-flex align-items-center justify-content-center py-4">
+    <div class="row justify-content-center w-100">
+        <div class="col-12 col-lg-10 col-xl-8">
+            <div class="home-panel">
+                <div class="home-panel-content text-center">
+                    <div class="home-icon mb-4">
+                        <i class="bi bi-journal-bookmark-fill"></i>
+                    </div>
+
+                    <h1 class="home-title mb-2">Escolha a Série</h1>
+                    <p class="home-subtitle mb-5">
+                        Selecione a etapa de ensino para iniciar o quiz.
+                    </p>
+
+                    <div class="row g-4 text-start mb-4">
+                        <div class="col-12 col-lg-6">
+                            <div class="grade-card h-100">
+                                <div class="grade-card-header text-center mb-4">
+                                    <h5 class="grade-card-title mb-0">Ensino Fundamental</h5>
+                                </div>
+
+                                <div class="d-grid gap-3">
+                                    <a href="{{ route('game.display', ['grade' => '6']) }}"
+                                        class="btn home-start-btn grade-btn">
+                                        6º Ano
+                                    </a>
+                                    <a href="{{ route('game.display', ['grade' => '7']) }}"
+                                        class="btn home-start-btn grade-btn">
+                                        7º Ano
+                                    </a>
+                                    <a href="{{ route('game.display', ['grade' => '8']) }}"
+                                        class="btn home-start-btn grade-btn">
+                                        8º Ano
+                                    </a>
+                                    <a href="{{ route('game.display', ['grade' => '9']) }}"
+                                        class="btn home-start-btn grade-btn">
+                                        9º Ano
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="col-12 col-lg-6">
+                            <div class="grade-card h-100">
+                                <div class="grade-card-header text-center mb-4">
+                                    <h5 class="grade-card-title mb-0">Ensino Médio</h5>
+                                </div>
+
+                                <div class="d-grid gap-3">
+                                    <a href="{{ route('game.display', ['grade' => '1']) }}"
+                                        class="btn home-start-btn grade-btn">
+                                        1º Ano
+                                    </a>
+                                    <a href="{{ route('game.display', ['grade' => '2']) }}"
+                                        class="btn home-start-btn grade-btn">
+                                        2º Ano
+                                    </a>
+                                    <a href="{{ route('game.display', ['grade' => '3']) }}"
+                                        class="btn home-start-btn grade-btn">
+                                        3º Ano
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <a href="{{ route('game.menu') }}" class="home-logout-link text-decoration-none">
+                        <i class="bi bi-arrow-left me-1"></i>
+                        Voltar
+                    </a>
+                </div>
+            </div>
         </div>
-        <div class="d-flex flex-column align-items-center mt-4">
-            <div class="col-12 col-md-4 mb-4">
-                <a href="{{ route('game.display', ['grade' => '6']) }}" class="btn btn-danger w-100 p-3 btn-default btn-raise">6º ANO</a>
-            </div>
-            <div class="col-12 col-md-4 mb-4">
-                <a href="{{ route('game.display', ['grade' => '7']) }}" class="btn btn-danger w-100 p-3 btn-default btn-raise">7º ANO</a>
-            </div>
-            <div class="col-12 col-md-4 mb-4">
-                <a href="{{ route('game.display', ['grade' => '8']) }}" class="btn btn-danger w-100 p-3 btn-default btn-raise">8º ANO</a>
-            </div>
-            <div class="col-12 col-md-4 mb-4">
-                <a href="{{ route('game.display', ['grade' => '9']) }}" class="btn btn-danger w-100 p-3 btn-default btn-raise">9º ANO</a>
-            </div>
-        </div>
-    </div>
-    <div class="col-md-9 mt-4">
-        <div class="d-flex justify-content-center mb-4">
-            <h5>ENSINO MÉDIO</h5>
-        </div>
-        <div class="d-flex flex-column align-items-center mt-4 mb-2">
-            <div class="col-12 col-md-4 mb-4">
-                <a href="{{ route('game.display', ['grade' => '1']) }}" class="btn btn-danger w-100 p-3 btn-default btn-raise">1º ANO</a>
-            </div>
-            <div class="col-12 col-md-4 mb-4">
-                <a href="{{ route('game.display', ['grade' => '2']) }}" class="btn btn-danger w-100 p-3 btn-default btn-raise">2º ANO</a>
-            </div>
-            <div class="col-12 col-md-4 mb-4">
-                <a href="{{ route('game.display', ['grade' => '3']) }}" class="btn btn-danger w-100 p-3 btn-default btn-raise">3º ANO</a>
-            </div>
-        </div>
-    </div>
-    <div class="row">
-        <a href="{{ route('game.menu') }}"
-            class="btn-exit text-decoration-none text-white w-100 p-1 position-relative d-inline-block overflow-hidden mt-4 mb-2">
-            <i class="bi bi-arrow-left icon" style="font-size: 20px; margin-top: 2px;"></i>
-            <span class="exit-text" style="font-size: 18px;">Voltar</span>
-        </a>
     </div>
 </div>
